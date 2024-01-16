6b89792dfc936bf2922b3251679579479bf81047
imbachelor <imbachelor@syneforge.com>
2024-01-16 18:36:41 +0200

    Adding and combining views

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by iambachelor on 15.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
